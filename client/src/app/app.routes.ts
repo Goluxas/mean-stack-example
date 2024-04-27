9e3e80e4539dcff5f89c7ef3c366644586f50ff8
Douglas Storz <douglasstorz@gmail.com>
2024-04-27 02:51:05 -0400

    Wired Add Employee page into routes. Now works

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { EmployeesListComponent } from './employees-list/employees-list.component';
+import { AddEmployeeComponent } from './add-employee/add-employee.component';
 
 export const routes: Routes = [
     { path: '', component: EmployeesListComponent, title: 'Employees List' },
+    { path: 'new', component: AddEmployeeComponent, title: 'Add New Employee' },
 ];

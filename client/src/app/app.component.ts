a98d9a62d901b93293b239b127c3f6a81fe5dc08
Douglas Storz <douglasstorz@gmail.com>
2024-04-27 02:54:48 -0400

    testing minimizing imports in base app component

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { EmployeesListComponent } from './employees-list/employees-list.component';
+//import { EmployeesListComponent } from './employees-list/employees-list.component';
 import { MatToolbarModule } from '@angular/material/toolbar'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, EmployeesListComponent, MatToolbarModule],
+  imports: [RouterOutlet, MatToolbarModule],
+  // I was able to delete EmployeesListComponent from these imports and it still works
+  // maybe because it's imported/configured in the router?
+  // Possibly including it was a holdover from older Angular versions
   template: `
     <mat-toolbar>
       <span>Employees Management System</span>
